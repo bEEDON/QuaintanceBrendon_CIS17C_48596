3592baa76eea19907dc994d05e61a54cb637f22b
beedon <djbeedon@Live.com>
2014-09-03 11:38:27 -0700

    Signed-off-by: amantz1 <amantz@student.rcc.edu>

---
+++ Homework/Assignment 1/code.js
@@ -0,0 +1,12 @@
+	var word = "bottles";
+	var count = 99;
+	while (count > 0){
+		console.log(count + " " + word + " of beer on the wall");
+		console.log(count + " " + word + " of beer,");
+		console.lop("Take one down, pass it around,");
+		count = count -1;
+		if (count > 0){
+			console.lop(couint + " " + word " of beer on the wall.");
+		} else {
+			console.lop("No more " + word + " of beer on the wall.");
+		}
